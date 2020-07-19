bc1521958e07b037c49126a6761869262ef6029d
kisszpy <kisszpy@163.com>
2020-07-19 23:21:49 +0800

    modify

---
+++ cmake-build-debug/CMakeFiles/clang.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clang.dir/main.cpp.o"
+  "clang"
+  "clang.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/clang.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
